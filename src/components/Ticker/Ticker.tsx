ad8b05fe20c94b8ebe40af9b3fec1a3bd7b93d36
saidmukhamad <saidmukhamad@yandex.ru>
2022-09-04 23:32:03 +0300

    ticker with setTimeouts

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { database } from "./database/database";
 import "./ticker.css";
 
@@ -7,18 +7,55 @@ export { database } from "./database/database";
 type Props = {};
 
 function Ticker({}: Props) {
-  const [ticker, setTicker] = useState(database[0].split(" "));
+    const [ticker, setTicker] = useState({
+        text: database[3],
+        seconds: Math.floor(database[3].length / 17 + 4),
+    });
 
-  return (
-    <div className="ticker">
-      {/* {ticker.map((text, index) => (
+    useEffect(() => {
+        let meme = setTimeout(function animate(i = 0) {
+            console.log(i);
+            let seconds = Math.floor(database[i].length / 17 + 4);
+            setTicker({
+                text: database[i],
+                seconds,
+            });
+            if (i == database.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+            meme = setTimeout(animate.bind(null, i), ticker.seconds * 1000);
+        }, ticker.seconds * 1000);
+
+        return () => clearTimeout(meme);
+    }, []);
+
+    useEffect(() => {
+        let seconds = setTimeout(function hm() {
+            console.log("прошла секунда");
+            seconds = setTimeout(hm, 1000);
+        }, 1000);
+
+        return () => clearTimeout(seconds);
+    }, []);
+
+    console.log(ticker);
+    return (
+        <div className="ticker">
+            {/* {ticker.map((text, index) => (
         <p>{text} &nbsp;</p>
       ))} */}
-      <div className="ticker-container">
-        <div className="ticker-child">{database[0]}</div>
-      </div>
-    </div>
-  );
+            <div className="ticker-container">
+                <div
+                    className="ticker-child"
+                    style={{ animationDuration: `${ticker.seconds}s` }}
+                >
+                    {ticker.text}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Ticker;
